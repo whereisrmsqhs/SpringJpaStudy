f79ad13faa5ac8997b907aef74af9f9730a1c08c
whereisrmsqhs <whereisrmsqhs@gmail.com>
2022-08-10 18:25:09 +0900

    Association Mapping

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,49 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args){
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try{
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("member1");
+            em.persist(member);
+
+            team.addMember(member);
+
+            em.flush();
+            em.clear();
+
+            Member findMember = em.find(Member.class, member.getId());
+            List<Member> members = findMember.getTeam().getMembers();
+
+            for(Member m : members){
+                System.out.println("m = " + m.getUsername());
+            }
+            tx.commit();
+        } catch (Exception e){
+            tx.rollback();
+        }finally{
+            em.close();
+        }
+
+        //code
+        emf.close();
+    }
+}
